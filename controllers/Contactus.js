903cff4b2c146acfe80c39e6ccc70cbc9be9c51e
Pradeep Kumaresan <pradeep.kumaresan@htcindia.local>
2021-12-13 19:32:23 +0530

    added

--- controllers/Contactus.js
+++ controllers/Contactus.js
@@ -13,9 +13,22 @@ module.exports = {
         })
     },
     AddContact : (req,res,next) => {
-        Contact.create(req, (err, result) => {
-            if (err) errorHandler(req, res, err);
-            else successHandler(req, res, 'Contactus created successfully', result);
-        });    
+        // Contact.create(req, (err, result) => {
+        //     if (err) errorHandler(req, res, err);
+        //     else successHandler(req, res, 'Contactus created successfully', result);
+        // });    
+        try {
+            // Category.create(req, (err, result) => {
+            //     if (err) errorHandler(req, res, err);
+            //     else successHandler(req, res, 'Category created successfully', result);
+            // }); 
+            console.log(req.body , "=======REQ BODY=========");
+            
+            res.send("Successfully created")
+        }
+        catch (err) {
+            console.log(err , "Error on contACT US CREATE")
+            res.send("Failed to store")
+        }
     }
 }
