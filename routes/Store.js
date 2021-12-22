e5c4d60cca49fee9d6d12cc26b6db9e9a1936e1e
narandhran <narandhran@gmail.com>
2021-12-22 12:51:24 +0530

    pug edited

--- routes/Store.js
+++ routes/Store.js
@@ -2,5 +2,5 @@ const storeCtl = require('../controllers/Store');
 
 module.exports = app => {
     app.get('/store/fetch/:pincode', storeCtl.fetchByPin);
-    app.get('/store/view_map/:pincode', storeCtl.viewMapByPin);
+    app.get('/store/viewmap/:pincode', storeCtl.viewMapByPin);
 }
